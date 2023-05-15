8e5470a7fc292f5a414597171e97cc5b144aa0ab
Victor Egret <victor.egret.pro@gmail.com>
2023-05-15 18:17:32 +0200

    cd better behavior

--- srcs/builtins/cd/cd.c
+++ srcs/builtins/cd/cd.c
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:37:59 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/05/15 18:15:53 by vegret           ###   ########.fr       */
+/*   Updated: 2023/05/15 18:17:11 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	builtin_cd(t_msh *msh, char **args)
 		return (1);
 	}
 	path = get_val(msh->env, "HOME");
-	if (args[1])
-		path = args[1];
 	if (!path)
 	{
 		ft_dprintf(2, "bash: cd: HOME not set\n");
 		return (1);
 	}
+	if (args[1])
+		path = args[1];
 	if (path[0] && chdir(path) != 0)
 	{
 		ft_dprintf(2, "bash: cd %s: %s\n", path, strerror(errno));
