251a72383b902da99963ec0f501e2568a7679a0b
Victor Egret <victor.egret.pro@gmail.com>
2023-06-13 16:43:34 +0200

    Printf protected in echo

--- srcs/builtins/echo/echo.c
+++ srcs/builtins/echo/echo.c
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:05:14 by vegret            #+#    #+#             */
-/*   Updated: 2023/06/13 15:48:31 by vegret           ###   ########.fr       */
+/*   Updated: 2023/06/13 16:43:07 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,27 @@ static bool	is_flag_n(char *s)
 	return (true);
 }
 
+static int	print_args(char **args, bool newline, int i)
+{
+	while (args[i])
+	{
+		if (printf("%s", args[i++]) < 0)
+			return (EXIT_FAILURE);
+		if (args[i] != NULL)
+		{
+			if (printf(" ") < 0)
+				return (EXIT_FAILURE);
+		}
+	}
+	if (newline && printf("\n") < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
 int	builtin_echo(t_msh *msh, char **args)
 {
-	int		i;
-	bool	newline;
+	int	i;
+	int	ret;
 
 	(void) msh;
 	if (!args || !args[0])
@@ -35,14 +52,8 @@ int	builtin_echo(t_msh *msh, char **args)
 	i = 1;
 	while (is_flag_n(args[i]))
 		i++;
-	newline = i == 1;
-	while (args[i])
-	{
-		printf("%s", args[i++]);
-		if (args[i] != NULL)
-			printf(" ");
-	}
-	if (newline)
-		printf("\n");
-	return (0);
+	ret = print_args(args, i == 1, i);
+	if (ret == EXIT_FAILURE)
+		perror("bash: echo: printf failed\n");
+	return (ret);
 }
