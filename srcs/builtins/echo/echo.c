41b068d16924827373c7c8dd16690887d12ba039
Victor Egret <victor.egret.pro@gmail.com>
2023-06-13 15:53:30 +0200

    Echo flag fix

--- srcs/builtins/echo/echo.c
+++ srcs/builtins/echo/echo.c
@@ -6,24 +6,36 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 23:05:14 by vegret            #+#    #+#             */
-/*   Updated: 2023/05/08 23:30:23 by vegret           ###   ########.fr       */
+/*   Updated: 2023/06/13 15:48:31 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	is_flag_n(char *s)
+{
+	if (s == NULL || *s++ != '-')
+		return (false);
+	if (*s == '\0')
+		return (false);
+	while (*s)
+		if (*s++ != 'n')
+			return (false);
+	return (true);
+}
+
 int	builtin_echo(t_msh *msh, char **args)
 {
-	int	i;
-	int	newline;
+	int		i;
+	bool	newline;
 
 	(void) msh;
 	if (!args || !args[0])
 		return (printf("\n"), 0);
-	newline = 1;
-	if (args[1] && ft_strncmp("-n", args[1], 3) == 0)
-		newline = 0;
-	i = 1 + !newline;
+	i = 1;
+	while (is_flag_n(args[i]))
+		i++;
+	newline = i == 1;
 	while (args[i])
 	{
 		printf("%s", args[i++]);
