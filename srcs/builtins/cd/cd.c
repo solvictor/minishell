d3eda0862e92c2b290374241c860abc67557931b
Victor Egret <victor.egret.pro@gmail.com>
2023-07-08 15:30:09 +0200

    cd WIP

--- srcs/builtins/cd/cd.c
+++ srcs/builtins/cd/cd.c
@@ -6,34 +6,88 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:37:59 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/07/05 16:29:25 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/07/08 15:24:25 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	update_env(t_env *env, char *cwd)
+{
+	t_env	*pwd;
+	t_env	*oldpwd;
+
+	pwd = get_env(env, "PWD", 3);
+	oldpwd = get_env(env, "OLDPWD", 6);
+	if (oldpwd)
+	{
+		free(oldpwd->var);
+		if (pwd)
+			oldpwd->var = ft_strjoin("OLDPWD=", pwd->var + 4);
+		else
+			oldpwd->var = ft_strdup("\0");
+		if (!oldpwd->var)
+			return (1);
+	}
+	if (pwd)
+	{
+		if (!cwd)
+			return (1);
+		free(pwd->var);
+		pwd->var = ft_strjoin("PWD=", cwd);
+		free(cwd);
+		if (!pwd->var)
+			return (1);
+	}
+	return (0);
+}
+
+static char	*get_path(t_env *env, char *arg, bool go_oldpwd)
+{
+	const bool	go_home = arg == NULL;
+	char		*var;
+
+	if (go_home || go_oldpwd)
+	{
+		var = "HOME";
+		if (go_oldpwd)
+			var = "OLDPWD";
+		arg = get_env_val(env, var);
+		if (!arg)
+		{
+			ft_dprintf(STDERR_FILENO, "minishell: cd: %s not set\n", var);
+			return (NULL);
+		}
+	}
+	if (*arg == '\0')
+		return (".");
+	return (arg);
+}
+
+// TODO Test env modif
 int	builtin_cd(t_msh *msh, char **args)
 {
 	char	*path;
+	bool	go_oldpwd;
 
 	if (args[1] && args[2])
 	{
 		ft_dprintf(STDERR_FILENO, "minishell: cd: too many arguments\n");
 		return (1);
 	}
-	path = get_env_val(msh->env, "HOME");
+	go_oldpwd = args[1] && ft_strncmp(args[1], "-\0", 2) == 0;
+	path = get_path(msh->env, args[1], go_oldpwd);
 	if (!path)
-	{
-		ft_dprintf(STDERR_FILENO, "minishell: cd: HOME not set\n");
 		return (1);
-	}
-	if (args[1])
-		path = args[1];
-	if (path[0] && chdir(path) != 0)
+	if (chdir(path) != 0)
 	{
 		ft_dprintf(STDERR_FILENO, "minishell: cd %s: %s\n", path,
 			strerror(errno));
 		return (1);
 	}
+	if (update_env(msh->env, getcwd(NULL, 0)))
+		return (1);
+	if (go_oldpwd)
+		return (builtin_pwd(msh, args));
 	return (0);
 }
--- srcs/builtins/pwd/pwd.c
+++ srcs/builtins/pwd/pwd.c
@@ -6,7 +6,7 @@
 /*   By: vegret <victor.egret.pro@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:46:30 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/07/05 16:32:17 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/07/07 15:10:31 by vegret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int	builtin_pwd(t_msh *msh, char **args)
 	char	*cwd;
 
 	(void) msh;
-	(void) args;
 	cwd = getcwd(NULL, 0);
 	if (cwd == NULL)
 	{
-		ft_dprintf(STDERR_FILENO, "minishell: pwd: %s\n", strerror(errno));
+		ft_dprintf(STDERR_FILENO, "minishell: %s: %s\n",
+			args[0], strerror(errno));
 		return (1);
 	}
 	if (printf("%s\n", cwd) < 0)
 	{
-		ft_dprintf(STDERR_FILENO, "minishell: pwd: write error: %s\n",
-			strerror(errno));
+		ft_dprintf(STDERR_FILENO, "minishell: %s: write error: %s\n",
+			args[0], strerror(errno));
 		free(cwd);
 		return (1);
 	}
